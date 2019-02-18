88158805f4903dec0c7d617852ef2bfe040c75d2
Metxnbr Lucccy <le2op7y3vc859@foxmail.com>
2019-02-18 19:29:08 +0800

    update

--- docs/jsonp/src/server2.js
+++ docs/jsonp/src/server2.js
@@ -20,4 +20,31 @@ app.get('/elsa', disableCross, (req, res)=>{
     res.jsonp({say: 'hi, i am Elsa'})
 })
 
+const OTHERS = [
+    'Kristoff',
+    'Olaf',
+    'Hans',
+]
+
+app.get('/other', disableCross, (req, res)=>{
+    const { name } = req.query
+
+    if(!name) {
+        res.jsonp({status: 'failed', say: "sorry, I don't know what you said"})
+        return
+    }
+
+    let n = name.trim()
+    n = n.toLowerCase()
+
+    const i = OTHERS.map(item=>item.toLowerCase()).indexOf(n)
+
+    if(i === -1) {
+        res.jsonp({status: 'failed', say: `${name} is not here`})
+        return
+    }
+
+    res.jsonp({status: 'success', say: `hi, i am ${OTHERS[i]}`})
+})
+
 app.listen(3002)
